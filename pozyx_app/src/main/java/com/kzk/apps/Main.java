043880a2f499702743942e792485b1900851109d
yuta <yuta@ai.iit.tsukuba.ac.jp>
2018-10-09 18:07:24 +0900

    Refactoring

--- pozyx_app/src/main/java/com/kzk/apps/Main.java
+++ pozyx_app/src/main/java/com/kzk/apps/Main.java
@@ -14,60 +14,33 @@ import com.kzk.libs.definitions.Registers;;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("hello world");
 		
-		PozyxSerial lib = new PozyxSerial(PozyxSerial.getFirstPozyxSerialPort());
+		// make an instance for serial communication.
+		PozyxSerial port = new PozyxSerial(PozyxSerial.getFirstPozyxSerialPort());
+		
+		// make an instance for the container of device data.
 		DeviceDetailes device = new DeviceDetailes();
-		lib.getDeviceDetails(device);
-		lib.deviceCheck(device);
-		lib.setLed(1, true);  // ?
-		lib.setLed(2, false);  // control for left LED, indicating with gree LED
-		lib.setLed(3, false);  // control for middle LED
-		lib.setLed(4, true);  // ?
 		
+		// to collect the device data from pozyx via Serial comunication
+		port.getDeviceDetails(device);
+		port.deviceCheck(device);  // print out the data.
+		
+		// LED control
+		// on 1 and 4 do not work on Pozyx Anchor.
+		port.setLed(1, true);  // ?
+		port.setLed(2, false);  // control for left LED, indicating with gree LED
+		port.setLed(3, false);  // control for middle LED
+		port.setLed(4, true);  // ?
+		
+		// Acceleration data 
 		Data ACCdata = new Data();  // build a container for ACC data.
-		lib.getACCData(ACCdata);
-		System.out.println(ACCdata.getData());		
-		System.out.println(ACCdata.getACCData(0));
-		System.out.println(ACCdata.getACCData(1));
-		System.out.println(ACCdata.getACCData(2));
+		port.getACCData(ACCdata);  // collect the device data from pozyx via Serial comunication
 		
-		String byteStr = "0A12BA";
-		byte[] test = {0x0A, 0x12, (byte) 0xBA};
-		
-		 int len = byteStr.length();
-	   byte[] data = new byte[len / 2];
-	   for (int i = 0; i < len; i += 2) {
-	       data[i / 2] = (byte) ((Character.digit(byteStr.charAt(i), 16) << 4)
-	                            + Character.digit(byteStr.charAt(i+1), 16));
-	   }
-		
-//		byte byteTest = 0x12;
-//		byte byteT = (0x01 << 6) | (((0x1 << 2) & (byte)0xFF));
-//	   byte byteData = (byte) ((0x01 << bShift1) | (((0x1 << bShift2) & (byte)0xFF)));
-//		System.out.println(String.format("%02x", byteT).toUpperCase());
-		String byteT = "D,F1";
-		System.out.println(Integer.parseInt(byteT.substring(2, 4), 16));
-		
-		
-//		System.out.println(byteStr.getBytes()[0]);
-//		System.out.println(test[2]);
-//		System.out.println(data[2]);
-//		PozyxSerial.listSerialPort();
-		
-//		ArrayList<String> test = PozyxSerial.getPozyxPorts();
-//		System.out.println(test.size());
-//		System.out.println(System.getProperty("os.name").toLowerCase()); // out the os name on this pc
-		
-		
-		byte verByte = 0x11;
-		String verString = "1A";
-		byte[] ver = Generic.hexStringToByteArray(verString);
-		
-		System.out.println("Byte: " + (verByte >> 4) + "   " + (verByte % 0x10));
-		
-		System.out.println("Byte: " + (ver[0] >> 4) + "   " + (ver[0] % 0x10));
+		System.out.println(ACCdata.getData());		// x, y, z
+		System.out.println(ACCdata.getACCData(0));  // x
+		System.out.println(ACCdata.getACCData(1));  // y
+		System.out.println(ACCdata.getACCData(2));  // z
 		
 	}
 }

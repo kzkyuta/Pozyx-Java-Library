364d6ac9d1537e69a3551de73a170f1c13fff3f0
小崎 勇太 <kozakiyuta@LAB-N1494.local>
2019-03-05 12:59:48 +0900

    add comment

--- pozyx_lib/src/main/java/com/kzk/libs/structures/generic/UWBSetting.java
+++ pozyx_lib/src/main/java/com/kzk/libs/structures/generic/UWBSetting.java
@@ -8,7 +8,7 @@ public class UWBSetting extends ByteStructure {
 	public static final byte DEFAULT_BITRATE = 0x01; // 0: 110kbits/s, 1: 850kbits/s(Default), 2: 6.8Mbits/s
 	public static final byte DEFAULT_PRF = 0x02; // 1: 16MHz;, 2: 64MHz(default)
 	public static final byte DEFAULT_PLEN = 0x08; // 1024 symbols
-	public static final double DEFAULT_GAIN_DB = 25.0;
+	public static final double DEFAULT_GAIN_DB = 25.0; // Do not touch
 	
 	protected String data = "";
 	protected int dataSize = 7;

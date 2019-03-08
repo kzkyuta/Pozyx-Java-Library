52e1d661180a6ea1232f81dee9820acf59494e09
小崎 勇太 <kozakiyuta@LAB-N1494.local>
2019-03-08 15:28:47 +0900

    delete codes in Data.java

--- pozyx_lib/src/main/java/com/kzk/libs/structures/generic/Data.java
+++ pozyx_lib/src/main/java/com/kzk/libs/structures/generic/Data.java
@@ -47,38 +47,4 @@ public class Data extends ByteStructure {
 	public String getFormat() {
 		return this.dataFormat;
 	}
-	
-	/////////
-
-//	public int getDataSize() {
-//		return dataSize;
-//	}
-//
-//	public void setDataSize(int dataSize) {
-//		this.dataSize = dataSize;
-//	}
-	
-	public int getACCData(int a) {
-		if(this.data.size() != 6) {
-			System.out.println("This data is not for ACC");
-			return 0;
-		}
-		int outData = 0;
-		switch(a) {
-			case 0:
-//				outData = Integer.parseInt(this.getData().substring(0, 4), 16);
-				outData = Integer.parseInt((this.getData(0) + this.getData(1)), 16);
-				break;
-			case 1:
-//				outData = Integer.parseInt(this.getData().substring(4, 8), 16);
-				outData = Integer.parseInt((this.getData(2) + this.getData(3)), 16);
-				break;
-			case 2:
-//				outData = Integer.parseInt(this.getData().substring(8, 12), 16);
-				outData = Integer.parseInt((this.getData(4) + this.getData(5)), 16);
-				break;
-		}
-		return outData;
-	}
-
 }

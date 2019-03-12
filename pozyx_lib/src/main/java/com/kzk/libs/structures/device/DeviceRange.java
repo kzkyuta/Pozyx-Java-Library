75eaf58509befa4bc30761d3773fa53aa064057d
小崎 勇太 <kozakiyuta@LAB-N1494.local>
2019-03-12 17:20:30 +0900

    add comments

--- pozyx_lib/src/main/java/com/kzk/libs/structures/device/DeviceRange.java
+++ pozyx_lib/src/main/java/com/kzk/libs/structures/device/DeviceRange.java
@@ -1,6 +1,7 @@
 package com.kzk.libs.structures.device;
 
 import com.kzk.libs.structures.ByteStructure;
+import com.kzk.libs.structures.generic.Data;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,9 +24,14 @@ public class DeviceRange extends ByteStructure{
     
     public DeviceRange() {
     	this.dataFormat = "bIIh";
+    	// b: returned byte which indicates success or not for function.
+    	// I: Time stamp
+    	// I: Last measurement range in mm.
+    	// h: signal Strength 
     	this.timestamp = 0;
     	this.distance = 0;
     	this.rss = 0;
+    	this.setPackedSize();
     }
     
     public DeviceRange(int timestamp, int distance, int rss) {
@@ -36,14 +42,16 @@ public class DeviceRange extends ByteStructure{
     }
     
     public void load(ArrayList<String> receivedData) {
-    	loadData(receivedData);
+    	data = receivedData;
     	this.timestamp = convertByteDataToInt(1);
     	this.distance = convertByteDataToInt(2);
     	this.rss = convertByteDataToInt(3);
 	}
+    
     public void updateData() {
     	
     }
+    
     public void showDeviceRange() {
 		System.out.println(this.timestamp + "ms, " + this.distance + "mm, " + this.rss + "dBm");
     }
